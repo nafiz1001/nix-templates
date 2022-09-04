bcdbb193f5f7354f931b6d44e6d78ec988c1b2e9
Nafiz Islam <nafiz.islam1001@gmail.com>
2022-09-04 11:16:04 -0400

    add javascript

---
+++ javascript/flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            nodejs
+            nodePackages.npm
+          ];
+        };
+      });
+}
